a157452c524da72a9dfb8e3e0fec0664b309d681
suryanshuPal1 <suryanshupal7@gmail.com>
2025-02-10 20:01:22 +0530

    email verification page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import AccountVerification from './pages/AccountVerification.jsx'
+import EmailVerificaton from './pages/EmailVerification.jsx'
 
 const App = () => {
   return (
     <>
     <AccountVerification/>
+    <EmailVerificaton/>
     </>
     
   )
