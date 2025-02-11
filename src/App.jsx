4f237a9c38fa1d1df92d13a9871840259da7125d
suryanshuPal1 <suryanshupal7@gmail.com>
2025-02-11 17:26:40 +0530

    code improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
 import './App.css'
-import ResetPassword from './components/Password/ResetPassword'
-import ResetPasswordSuccess from './components/Password/ResetPasswordSuccess'
+import AccountVerification from './Pages/AccountVerification'
+import EmailVerification from './Pages/EmailVerification'
+import ResetPassword from './Pages/ResetPassword'
+import ResetPasswordSuccess from './Pages/ResetPasswordSuccess'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <section>
       <ResetPassword />
       <ResetPasswordSuccess />
+      <AccountVerification/>
+      <EmailVerification/>
     </section>
     
   )
