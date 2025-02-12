5fe69193a47cd5585ba9baeb611c0c99fe64f9d4
suryanshuPal1 <suryanshupal7@gmail.com>
2025-02-12 17:46:38 +0530

    complete sidebar is created

--- src/components/Password/Sidebar/Sidebar.jsx
+++ src/components/Password/Sidebar/Sidebar.jsx
@@ -6,6 +6,10 @@ import manageIcon from '../../../assets/Dashboard Icon/manage.png'
 import manageNewHeadlineIcon from '../../../assets/Dashboard Icon/manage-headline.png' 
 import manageNewarticleIcon from '../../../assets/Dashboard Icon/manage-article.png' 
 import manageVideoIcon from '../../../assets/Dashboard Icon/manage-video.png' 
+import profileIcon from '../../../assets/Dashboard Icon/profile.png'
+import setting from '../../../assets/Dashboard Icon/setting.png'
+import profile from '../../../assets/searchBarIcon/Ellipse.png'
+import logout from '../../../assets/Dashboard Icon/logout.png'
 
 const Sidebar = () => {
   const [ dropManage,setDropManage] = useState(false);
@@ -20,13 +24,9 @@ const Sidebar = () => {
   return (
     <>
     <div className='flex flex-col lg:h-200 w-[17%] bg-[#1C2059] text-white items-center lg:pt-40 fixed left-0'>
+      <div className=' w-full pb-3'>
+        <div href="dashbord" className='hover:bg-[#FFAC06] flex flex-row mt-4 px-5 py-2'><span className='mr-4'><img src={dashboardIcon} alt="dashboard Icon" /></span>Dashboard</div>      
 
-
-      <div className=' w-full'>
-
-        <div href="dashbord" className='hover:bg-[#FFAC06] flex flex-row mt-4 px-5 py-2'><span className='mr-4'><img src={dashboardIcon} alt="dashboard Icon" /></span>Dashboard</div>
-        
-        
         <div className={`hover:bg-[#FFAC06] flex flex-row mt-4 px-5 py-2  ${dropNew ? 'bg-[#FFAC06]' : 'bg-white-0'}`} onClick={()=>dropNewHandler()}><span className='mr-4'><img src={newIcon} alt="" /></span>New</div>
         {dropNew && (<div className='flex flex-col pl-13 text-sm '>
           <Link to={'/Post-New-Headline'} className='hover:text-[#FFAC06] mt-2'>Post New Headline</Link>
@@ -34,7 +34,6 @@ const Sidebar = () => {
           <Link to={'/Post-Video'} className='hover:text-[#FFAC06] mt-2'>Post Video</Link>
         </div>)} 
 
-        
         <div className={`hover:bg-[#FFAC06] flex flex-row mt-4  px-5 py-2 ${dropManage ? 'bg-[#FFAC06]' : 'bg-white-0'}`} onClick={()=>dropManageHandler()}><span className='mr-4'><img src={manageIcon} alt="" /></span>Manage</div>
         {dropManage && (<div className='flex flex-col pl-13 text-sm'>
           <Link to={'/Manage-New-Headline'} className='flex flex-row hover:text-[#FFAC06] mt-2'><span className='mr-3'><img src={manageNewHeadlineIcon} alt="" className='hover:text-[#FFAC06]'/></span>Manage New Headline</Link>
@@ -42,13 +41,24 @@ const Sidebar = () => {
           <Link to={'/Manage-Video'} className='flex flex-row hover:text-[#FFAC06] mt-2'><span className='mr-3'><img src={manageVideoIcon} alt="" /></span>Manage Video</Link>
         </div>)} 
 
-        <div href="/profile" className='hover:bg-[#FFAC06] mt-4 px-5 py-2'>Profile</div>
+        <Link href="/profile" className='hover:bg-[#FFAC06] mt-4 px-5 py-2 flex flex-row'><span className='mr-4'><img src={profileIcon} alt="" /></span>Profile</Link>
 
-        <div href="/setting" className='hover:bg-[#FFAC06] mt-4 px-5 py-2'>Setting</div>
+        <Link href="/setting" className='hover:bg-[#FFAC06] mt-4 px-5 py-2 flex flex-row'><span className='mr-4'><img src={setting} alt="" /></span>Setting</Link>
 
       </div>
-
-
+      <div className='border-t w-[90%]'>
+        <div className='flex flex-row py-5'>
+          <img src={profile} alt="" />
+          <div className='pl-2'>
+            <h2>News Paper</h2>
+            <p className='text-xs bg-[#FFCD71] text-black border rounded-lg pl-1'>Super Admin</p>
+          </div>
+        </div>
+        <div className='flex flex-row pl-1'>
+         <span><img src={logout} alt="" /></span> 
+        <button className='text-[#FFAC06] px-2'>Log Out</button>
+        </div>
+      </div>
     </div>
     </>
   )
