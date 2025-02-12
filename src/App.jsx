4db3a84e544ba5e587d3a32c2936b93772106065
suryanshuPal1 <suryanshupal7@gmail.com>
2025-02-12 15:43:50 +0530

    sidebar with routhing functionality and search navbar both component are created with manage headline and manage article page are also created

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
+import { Routes,Route } from 'react-router-dom'
 import './App.css'
 import AccountVerification from './Pages/AccountVerification'
 import EmailVerification from './Pages/EmailVerification'
 import ResetPassword from './Pages/ResetPassword'
 import ResetPasswordSuccess from './Pages/ResetPasswordSuccess'
+import Sidebar from './components/Password/Sidebar/Sidebar'
+import SearchNavbar from './components/Password/Sidebar/SearchNavbar'
+
+import ManageHeadline from './Pages/ManageHeadline'
+import ManageArticle from './Pages/ManageArticle'
+import Footer from './components/footer'
 
 
 function App() {
   return (
-    <section>
-      <ResetPassword />
+    <section className='flex'>
+      <div className='w-[17%]'>
+        <Sidebar/>
+      </div>
+      <div className='w-[83%] '>
+        <SearchNavbar/>
+        <Routes>
+          <Route path='/Manage-New-Headline'element={<ManageHeadline/>}/>
+          <Route path='/Manage-New-Article' element={<ManageArticle/>}/>
+        </Routes>
+        <Footer/>
+      </div>
+      {/* <ResetPassword />
       <ResetPasswordSuccess />
       <AccountVerification/>
-      <EmailVerification/>
+      <EmailVerification/> */}
     </section>
     
   )
